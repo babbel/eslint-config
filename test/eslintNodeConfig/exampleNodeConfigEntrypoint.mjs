611789f5102a668774bc9324325201bd8953450b
Eric L. Goldstein <egoldstein@babbel.com>
2024-01-05 12:45:57 -0500

    add real Node test code

--- test/eslintNodeConfig/exampleNodeConfigEntrypoint.mjs
+++ test/eslintNodeConfig/exampleNodeConfigEntrypoint.mjs
@@ -1,1 +1,21 @@
-console.log('node');
+// External Imports
+import http from 'node:http';
+
+// Local Variables
+const hostname = '127.0.0.1';
+const port = 3_000;
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello World\n');
+});
+
+// Local Functions
+function startServer() {
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+
+// Begin Execution
+startServer();
